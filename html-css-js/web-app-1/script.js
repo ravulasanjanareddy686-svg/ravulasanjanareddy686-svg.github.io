d0bcd3ef5522558ed30068a0fd478e946b53a19f
ravulasanjanareddy686@gmail.com <ravulasanjanareddy686@gmail.com>
2026-01-10 11:03:14 +0530

    updated

---
+++ html-css-js/web-app-1/script.js
@@ -0,0 +1,7 @@
+function dispMessage(){
+    // a=10;
+    // b=20;
+    // c=a+b
+    // alert(document.getElementById("txtEmail").value)
+    lblMessage.innerHTML= document.getElementById("txtEmail").value
+}
