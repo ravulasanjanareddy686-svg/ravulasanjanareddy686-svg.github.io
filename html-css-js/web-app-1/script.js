1215f075bc683166e96c56ea184dee41bd8b7b40
ravulasanjanareddy686@gmail.com <ravulasanjanareddy686@gmail.com>
2026-01-10 11:30:20 +0530

    updated

--- html-css-js/web-app-1/script.js
+++ html-css-js/web-app-1/script.js
@@ -4,4 +4,5 @@ function dispMessage(){
     // c=a+b
     // alert(document.getElementById("txtEmail").value)
     lblMessage.innerHTML= document.getElementById("txtEmail").value
+    lbl2.innerHTML = document.getElementById("pass").value
 }
